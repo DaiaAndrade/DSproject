359477e1d2d6ad4b455144dae5b73e4b14d9fcb2
DaiaAndrade <daiafocking@gmail.com>
2015-09-27 22:57:39 -0300

    Adicionando coisas
    troca de cores, exemplos de elementos, js funcionando

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+$("#Link1").click(function(){
+	$("#MenuLinks").animate({height:"toggle"});
+});
+
+$("#Link2").click(function(){
+	$("#PerfilLinks").animate({height:"toggle"});
+});
+$(document).ready();
